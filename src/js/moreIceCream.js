8e356392498aaab134d7408adc6f205444b51edd
Sergii Bogush <sergbogush@gmail.com>
2021-02-24 09:49:15 +0200

    Fixed scroll

--- src/js/moreIceCream.js
+++ src/js/moreIceCream.js
@@ -1,43 +1,23 @@
-// function readMore() {
-//     var dot = document.getElementById("dot");
-//     var moreIce = document.getElementById("moreIce");
-//     var iceCreamBtn = document.getElementById("iceCreamBtn");
-
-//     if (dot.style.display === "none") {
-//         dot.style.display = "inline";
-//         iceCreamBtn.innerHTML="more";
-//         moreIce.style.display = "none";
-//     }
-//     else {
-//          dot.style.display = "none";
-//         iceCreamBtn.innerHTML="Confirm";
-//         moreIce.style.display = "block";
-//     }
-// }
-
-    document.getElementById('hider icecream').onclick = function () {
-      document.getElementById('read more icecream').hidden = true;
-    };
- 
-    
-    document.getElementById('opener icecream').onclick = function () {
-      document.getElementById('read more icecream').hidden = false;
-    };
-  
-    document.getElementById('hider coffee').onclick = function () {
-      document.getElementById('read more coffee').hidden = true;
-    };
- 
-    document.getElementById('opener coffee').onclick = function () {
-      document.getElementById('read more coffee').hidden = false;
-    };
-  
-    document.getElementById('hider milkshake').onclick = function () {
-      document.getElementById('read more milkshake').hidden = true;
+document.getElementById('btn_icecream').onclick = function () {
+  document.getElementById('icecream').hidden = false;
 };
-    document.getElementById('opener milkshake').onclick = function () {
-      document.getElementById('read more milkshake').hidden = false;
-    };
-
+document.getElementById('input_icecream').onclick = function () {
+  document.getElementById('icecream').hidden = true;
+};
+document.getElementById('btn_coffee').onclick = function () {
+  document.getElementById('coffee').hidden = false;
+};
+document.getElementById('input_coffee').onclick = function () {
+document.getElementById('coffee').hidden = true;
   
+};
+
+document.getElementById('btn_milkshake').onclick = function () {
+document.getElementById('milkshake').hidden = false;
+};
+
+document.getElementById('input_milkshake').onclick = function () {
+document.getElementById('milkshake').hidden = true;
+};
+
 
